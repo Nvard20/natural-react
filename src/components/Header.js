80c2e9b7bfb01d6e7070b91a83a33703eb872099
nvard20 <nvard.asatryan96@gmail.com>
2021-03-22 15:23:10 +0400

    navbar fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
 import React, { useEffect,useState } from 'react';
 import {  Link, Switch,useHistory,NavLink } from "react-router-dom";
+
 export default function Header() {
 
 const [click,setClick] = useState(false);
+const [navbar,setNavbar] = useState(false);
 const handleClick = () => setClick(!click);
 const closeMobile = () =>setClick(false);
+
+const Changebackground = () =>{
+   
+
+    if( window.scrollY > 90 ) {
+      
+      
+          setNavbar(true);
+    }else{
+        setNavbar(false);
+    }
+};
+window.addEventListener('scroll',Changebackground);
+
     return (
-        <div className='navbar'>
+        <div className={ navbar ? 'navbar active-navbar' : 'navbar'}>
             <div className="logo"> <NavLink to="/"  exact  className="logo-img"><img src="img/logo.png"/></NavLink></div>
             <ul className='list'>
         
